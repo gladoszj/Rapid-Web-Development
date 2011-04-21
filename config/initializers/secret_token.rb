312f9e546350d0e13d468d9e4f06e476b0f1ed54
Adam Oldak <adam.oldak@roche.com>
2011-04-21 09:42:55 +0200

    Fresh TorqueBox/Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookARoom::Application.config.secret_token = 'd6654351a0e99f343b22396f342923afebb374e2de20775e1f2f2b67a6fe2e7198387f591506e5c37d5e96045520d6037f8d23ca7aeeaf2afb603460ea9c740e'
